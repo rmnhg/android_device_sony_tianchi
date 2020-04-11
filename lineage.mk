ba5534188f41d683a11d0363bd14db8949bc90c1
infus38 <infus38@gmail.com>
2020-04-11 19:03:20 +0200

    tianchi: rebrand to lineageOS
    Signed-off-by: infus38 <infus38@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -14,13 +14,15 @@
 
 TARGET_KERNEL_CONFIG := aosp_yukon_tianchi_defconfig
 
+# Inherit lineage common stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/tianchi/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := aosp_d5303
+PRODUCT_NAME := lineage_tianchi
 PRODUCT_DEVICE := tianchi
-PRODUCT_MODEL := Xperia T2 Ultra (AOSP)
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
